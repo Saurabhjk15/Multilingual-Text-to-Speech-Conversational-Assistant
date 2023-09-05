66567d7c25f05ba7046fb36e14675ee77cf3d096
Saurabh sharma <142724580+Saurabhjk15@users.noreply.github.com>
2023-09-05 12:18:50 +0530

    Add files via upload

---
+++ speaker.py
@@ -0,0 +1,32 @@
+import pyttsx3
+command = pyttsx3.init()
+voices = command.getProperty('voices')
+for voice in voices:
+    print("Voice:", voice.name)
+
+
+def speak():
+    lang_choice = input("Enter the language code (e.g., en for English, es for Spanish,ar for arabic): ")
+
+speak()
+
+
+
+print("Welcome to your personal speaker")
+
+while True:
+        a = input("type your thoughts here")
+        if a == "#":
+            command.say("goodbye , my friend")
+            command.runAndWait()
+            break
+        command.say(a)
+        command.runAndWait()
+
+
+
+
+
+
+
+
